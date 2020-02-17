354d01716f4335aab90b3a16cbe896e8e39aa257
Peerapathananont, Kajornsak (Agoda) <kajornsak.peerapathananont@agoda.com>
2020-02-18 02:20:56 +0700

    todo_step_3

--- lib/todo_item.dart
+++ lib/todo_item.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 class TodoItem extends StatefulWidget {
+  final Todo todo;
+
+  const TodoItem(this.todo);
+
   @override
   _TodoItemState createState() => _TodoItemState();
 }
 
 class _TodoItemState extends State<TodoItem> {
-  bool checked = false;
+  void onCheckboxChanged(bool value) {
+    setState(() {
+      widget.todo.checked = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +23,29 @@ class _TodoItemState extends State<TodoItem> {
       child: Row(
         children: <Widget>[
           Checkbox(
-            value: checked,
-            onChanged: (bool value) {
-              setState(() {
-                checked = value;
-              });
-            },
+            value: widget.todo.checked,
+            onChanged: onCheckboxChanged,
           ),
-          Text("TODO")
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(widget.todo.title,
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              if(widget.todo.subtitle != "")
+                Text(widget.todo.subtitle,
+                    style: TextStyle(color: Colors.grey[500]))
+            ],
+          )
         ],
       ),
     );
   }
+}
+
+class Todo {
+  String title = "";
+  String subtitle = "";
+  bool checked = false;
+
+  Todo(this.title, this.subtitle, {this.checked = false});
 }
